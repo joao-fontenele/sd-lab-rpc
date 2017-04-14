f1d824dac42872870a1b731575105f7de947b2f9
jpaulofb <jpfb@icomp.ufam.edu.br>
2017-04-14 11:49:29 -0400

    fix: removing trailing whitespace

--- src/icomp/client/IMCClient.java
+++ src/icomp/client/IMCClient.java
@@ -17,7 +17,7 @@ public class IMCClient extends Client {
 		}
 		String serverIP = args[0];
 		int port = (int) new Integer(args[1]);
-		
+
 		// this message has weight, height and sex, serialized, using the server's
 		// protocol
 		String requestMessage = IMCClient.readInputsFromKeyBoard();
@@ -28,7 +28,7 @@ public class IMCClient extends Client {
 		System.out.println("Requisitando o IMC ...");
 		String response = client.requestIMC(requestMessage);
 		System.out.println(response);
-		
+
 		System.out.println("\nFechando conexão com o servidor ...");
 		client.closeConnection();
 	}
@@ -36,7 +36,7 @@ public class IMCClient extends Client {
 	public IMCClient(String serverIP, int port) throws IOException {
 		super(serverIP, port);
 	}
-	
+
 	/*
 	 * Local method, read weight, height and sex from keyboard and serializes the
 	 * inputs into a string to be sent to the server. Using the protocol mentioned
@@ -50,14 +50,14 @@ public class IMCClient extends Client {
 
 		System.out.println("Digite sua altura em m (e.g. 1.7): ");
 		String height = scanner.nextLine();
-		
+
 		System.out.println("Digite seu sexo (e.g. M | F): ");
 		String sex = scanner.nextLine();
-		
+
 		scanner.close();
 		return IMCClient.serializeRequest(weight, height, sex);
 	}
-	
+
 	public static String serializeRequest(String weight, String height, String sex) {
 		return weight + SEP + height + SEP + sex + SEP + SEP;
 	}
@@ -68,16 +68,16 @@ public class IMCClient extends Client {
 	public String requestIMC(String message) throws IOException {
 		DataOutputStream out = this.getOutputStream();
 		out.writeChars(message);
-		
+
 		String messagesEnd = SEP + SEP; // signal the end of the server's response
-		
+
 		String response = "";
 		DataInputStream in =  this.getInputStream();
-		
+
 		while (!response.endsWith(messagesEnd)) {
 			response += in.readChar();
 		}
-		
+
 		return response;
 	}
 }
